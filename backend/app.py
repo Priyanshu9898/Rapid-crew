5c9a7353bb0b569238428e05b28ad34dfe8d9b88
Priyanshu Malaviya <priyanshumalaviya17@gmail.com>
2022-05-29 15:47:49 +0530

    100% Done

--- backend/app.py
+++ backend/app.py
@@ -2,102 +2,119 @@ from flask import Flask, request, url_for
 import numpy as np
 import pandas as pd
 import pickle
-# import re
-# from io import BytesIO
-# import base64
-# from PIL import Image
-# import urllib
-# from sklearn.neighbors import NearestNeighbors
-# from numpy.linalg import norm
-# import tensorflow
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.layers import GlobalMaxPooling2D
-# from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+import re
+from io import BytesIO
+import base64
+from PIL import Image
+import urllib
+from sklearn.neighbors import NearestNeighbors
+from numpy.linalg import norm
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 
 
 app = Flask(__name__)
 
+
+# Load Myntra Dataset
 myntra = pd.read_csv('myntra.csv')
 
+# Sig file which contains similarity between each product
 sig = pickle.load(open('sig.pkl', 'rb'))
+
+# Indices to get product title
 indices = pickle.load(open('indices.pkl', 'rb'))
-# embeddings = np.array(pickle.load(open('embeddings.pkl', 'rb')))
 
+# features of each product image in our dataset
+embeddings = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+
+
+# all popular products in men's category
 men_popular = pickle.load(open('men_popular.pkl', 'rb'))
-
+# all popular products in women's category
 women_popular = pickle.load(open('women_popular.pkl', 'rb'))
 
-
+# all popular products in 
 popular_products = pickle.load(open('popular_products.pkl', 'rb'))
+
 
 filtered_indices = np.array(pickle.load(open('filtered_indices.pkl', 'rb')))
 
 
-# model = ResNet50(weights='imagenet', include_top=False,
-#                  input_shape=(224, 224, 3))
-# model.trainable = False
+# resnet model to train uploaded images
+model = ResNet50(weights='imagenet', include_top=False,
+                 input_shape=(224, 224, 3))
+model.trainable = False
 
-# model = tensorflow.keras.Sequential([
-#     model,
-#     GlobalMaxPooling2D()
-# ])
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
 
-# print(model.summary())
-
-
-# def feature_extraction(img_array, model):
-
-#     expanded_img_array = np.expand_dims(img_array, axis=0)
-#     # print(expanded_img_array.shape)
-#     preprocessed_img = preprocess_input(expanded_img_array)
-#     # print(preprocessed_img.shape)
-#     result = model.predict(expanded_img_array).flatten()
-#     # print(result.shape)
-#     # print(result / norm(result))
-#     return result / norm(result)
+print(model.summary())
 
 
-# def recommend(features, feature_list):
-#     neighbors = NearestNeighbors(
-#         n_neighbors=6, algorithm='brute', metric='euclidean')
-#     neighbors.fit(feature_list)
+# function to extract features from image
+def feature_extraction(img_array, model):
 
-#     distances, indices = neighbors.kneighbors([features])
+    # expand the dimention of image array
+    expanded_img_array = np.expand_dims(img_array, axis=0)
 
-#     return indices
+    preprocessed_img = preprocess_input(expanded_img_array)
 
+    # Get the features of image 
+    result = model.predict(expanded_img_array).flatten()
+    
+    return result / norm(result)
 
+# Recommand sililar feature products
+def recommend(features, feature_list):
+    neighbors = NearestNeighbors(
+        n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors.fit(feature_list)
+
+    distances, indices = neighbors.kneighbors([features])
+
+    return indices
+
+# Return all best selling products
 @app.route('/bestsellers', methods=['GET'])
 def get_data():
 
     return popular_products.to_json(orient='records')
 
-
+# return all best selling products in men's category
 @app.route('/menProducts', methods=['GET'])
 def get_men_data():
 
     return men_popular.to_json(orient='records')
 
-
+# return all best selling products in women's category
 @app.route('/womenProducts', methods=['GET'])
 def get_women_data():
     return women_popular.to_json(orient='records')
 
-
+# return all products data
 @app.route('/allProducts', methods=['GET'])
 def get_all_data():
     return myntra.to_json(orient='records')
 
-
+# Give similar products data based on title
 @app.route('/prod/<title>', methods=['GET'])
 def get_prod(title):
 
+    # get the index of product through title
     index = indices[title]
+
 
     sig_cs = list(enumerate(sig[index]))
 
+    # Sorting done based on similarity
     sig2 = sorted(sig_cs, key=lambda x: x[1], reverse=True)
 
+    # get 12 products
     sig_cs2 = sig2[1: 13]
 
     product_indices = [i[0] for i in sig_cs2]
@@ -105,6 +122,7 @@ def get_prod(title):
     return myntra.iloc[product_indices].to_json(orient='records')
 
 
+# Give buying recommandation of products based on title
 @app.route('/recommand/<title>', methods=['GET'])
 def get_recommand(title):
 
@@ -114,24 +132,30 @@ def get_recommand(title):
 
     return myntra.iloc[output].to_json(orient='records')
 
+# Return similar products based on image features
+@app.route('/imageData', methods=['GET', 'POST'])
+def get_image_data():
+    img_data = request.get_json()
 
-# @app.route('/imageData', methods=['GET', 'POST'])
-# def get_image_data():
-#     img_data = request.get_json()
-#     # print(img_data)
-#     img = img_data['data'][23:]
-#     im = Image.open(BytesIO(base64.b64decode(img))).resize((224, 224))
-#     img_array = np.array(im)
-#     # print(img_array)
-#     features = feature_extraction(img_array, model)
-#     print(features)
+    # image is in base64 format: so need to remove first 22 characters
+    img = img_data['data'][23:]
+    
+    # Load the image from base64 format and resize it to (224 x 224) so that it can be fed to the model
+    im = Image.open(BytesIO(base64.b64decode(img))).resize((224, 224))
+    
+    # Convert image into array
+    img_array = np.array(im)
+    # print(img_array)
+    
+    # Extract features from the image
+    features = feature_extraction(img_array, model)
+    print(features)
 
-#     indices = recommend(features, embeddings)
-#     # # print(type(indices[0]))
-#     # # print(type(indices))
-#     # # print((indices[0]))
+    # Get the similar products indices
+    indices = recommend(features, embeddings)
+    
 
-#     return myntra.iloc[indices[0]].to_json(orient='records')
+    return myntra.iloc[indices[0]].to_json(orient='records')
 
 
 if __name__ == '__main__':
